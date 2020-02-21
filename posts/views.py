4602c23fa0a150b185d3aac5f083868857c757d6
jod35 <jod35@gmail.com>
2020-02-21 12:03:38 -0600

    created the views

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView,ListView
 
-# Create your views here.
+
+class HomePageView(TemplateView):
+    template_name="index.html"
+
+class AboutPageView(TemplateView):
+    template_name="about.html"
+
+class LoginView(TemplateView):
+    template_name="login.html"
+
+class SignUpView(TemplateView):
+    template_name="signup.html"
