4dc3bd7b8772bba7b73c86132981d81aa99633c1
jod35 <jod35@gmail.com>
2020-02-21 13:12:29 -0600

    added the post and the user models

--- posts/models.py
+++ posts/models.py
@@ -1,2 +1,19 @@
 from django.db import models
 
+class User(models.Model):
+    username=models.CharField(max_length=200)
+    email=models.CharField(max_length=200)
+    password_hash=models.TextField()
+
+    def __str__(self):
+        return self.username
+
+class Post(models.Model):
+    title=models.CharField(max_length=300)
+    content=models.TextField()
+    author=models.ForeignKey(User,on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.title
+
