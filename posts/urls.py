a4988c3ae4be8b4bb74ad4558deb4644eb2234fb
jod35 <jod35@gmail.com>
2020-02-21 12:34:03 -0600

    created the templates

---
+++ posts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("",views.HomePageView.as_view(),name="site_home"),
+    path("about/",views.AddPostView.as_view(),name='site_about'),
+    path("login/",views.LoginView.as_view(),name="site_login"),
+    path("signup/",views.SignUpView.as_view(),name="site_signup")
+]
--- posts/views.py
+++ posts/views.py
@@ -2,13 +2,13 @@ from django.views.generic import TemplateView,ListView
 
 
 class HomePageView(TemplateView):
-    template_name="index.html"
+    template_name="posts/index.html"
 
-class AboutPageView(TemplateView):
-    template_name="about.html"
+class AddPostView(TemplateView):
+    template_name="posts/add.html"
 
 class LoginView(TemplateView):
-    template_name="login.html"
+    template_name="posts/login.html"
 
 class SignUpView(TemplateView):
-    template_name="signup.html"
+    template_name="posts/signup.html"
