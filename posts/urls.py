6fe264fdf3aeda3322daf0daa840af616a57cc5d
jona <jona@jona.com>
2020-02-21 14:29:11 -0500

    added the urls for creating a single post

--- posts/urls.py
+++ posts/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("",views.HomePageView.as_view(),name="site_home"),
     path("about/",views.AddPostView.as_view(),name='site_about'),
     path("login/",views.LoginView.as_view(),name="site_login"),
-    path("signup/",views.SignUpView.as_view(),name="site_signup")
+    path("signup/",views.SignUpView.as_view(),name="site_signup"),
+    path("add/",views.AddPostView.as_view(),name='posts/add.html')
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,14 @@
 from django.views.generic import TemplateView,ListView
-
+from django.views.generic.edit import CreateView
+from .models import Post,User
 
 class HomePageView(TemplateView):
     template_name="posts/index.html"
 
-class AddPostView(TemplateView):
+class AddPostView(CreateView):
+    model=Post
     template_name="posts/add.html"
+    fields='__all__'
 
 class LoginView(TemplateView):
     template_name="posts/login.html"
